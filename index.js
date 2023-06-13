2f364d4c54e4ac4c937824df070d3e7c50c5b886
NedAdmission <130411059+NedAdmission@users.noreply.github.com>
2023-06-14 00:28:54 +0500

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ dotenv.config()
 const app = express()
 app.use(express.json())
 app.use(cors({
-    origin:["https://www.nedadmissioncell.com","http://localhost:3000"]
+    origin:["https://www.nedadmissioncell.com","http://localhost:3000"],
     methods:["GET","POST","PUT","DELETE"]
 }))
 const connect = ()=>{
